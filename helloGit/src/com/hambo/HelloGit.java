83ef3f8dd4de4d553723f3651c065051f1aa20eb
shuaishuai <shuaishuai996@163.com>
2018-11-15 21:54:45 +0800

    修改1.0

--- helloGit/src/com/hambo/HelloGit.java
+++ helloGit/src/com/hambo/HelloGit.java
@@ -7,12 +7,14 @@ public class HelloGit {
                 System.out.print(j+"*"+i+"="+i*j+"\t");
             }
             System.out.println();
+            //克隆的项目
         }
         test();
     }
 
     public static void test(){
         System.out.println("Git是分布式版本控制系统~");
+        System.out.println("修改1.0");
     }
 
 }
